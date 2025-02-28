6032ec5d74596c2f034619f00723bd5f59856525
zetcgo <cjr202321@yonsei.ac.kr>
2025-02-28 23:18:27 +0900

    🔧 v0.7.7

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,13 @@ const js = require('@eslint/js');
 const globals = require('globals');
 const prettierLinter = require('eslint-plugin-prettier');
 const prettierConfigRecommended = require('eslint-plugin-prettier/recommended');
-const pugLinter = require('eslint-plugin-pug');
 
 module.exports = [
     js.configs.recommended,
     prettierConfigRecommended,
     {
         languageOptions: { globals: { ...globals.node } },
-        plugins: { prettierLinter, pugLinter },
+        plugins: { prettierLinter },
         rules: { 'prettier/prettier': 'warn' },
     },
 ];
