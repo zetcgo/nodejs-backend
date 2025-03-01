5f3fe5187371d9ffbc3ca9aa1909cf9cde15cb0b
zetcgo <cjr202321@yonsei.ac.kr>
2025-03-02 01:36:17 +0900

    🔧 Add Nest.js Configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 const js = require('@eslint/js');
+const ts = require('typescript-eslint');
 const globals = require('globals');
 const prettierLinter = require('eslint-plugin-prettier');
 const prettierConfigRecommended = require('eslint-plugin-prettier/recommended');
@@ -11,4 +12,18 @@ module.exports = [
         plugins: { prettierLinter },
         rules: { 'prettier/prettier': 'warn' },
     },
+    ...ts.configs.recommendedTypeChecked.map((config) => ({
+        ...config,
+        files: ['**/*.ts'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            ...config.languageOptions,
+            parserOptions: {
+                ...config.languageOptions?.parserOptions,
+                project: ['**/tsconfig.json'],
+                projectService: true,
+                tsconfigRootDir: __dirname,
+            },
+        },
+    })),
 ];
