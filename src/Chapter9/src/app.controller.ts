e6164d1db4973f298f0c313b1428493ce758feb1
zetcgo <cjr202321@yonsei.ac.kr>
2025-03-04 11:20:12 +0900

    🔧 Add config files

--- src/Chapter9/src/app.controller.ts
+++ src/Chapter9/src/app.controller.ts
@@ -9,4 +9,21 @@ export class AppController {
     greet(): string {
         return this.configService.get('GREETING');
     }
+
+    @Get('server-info')
+    getServerInfo(): string {
+        return this.configService.get('SERVER_URL');
+    }
+
+    @Get('db-info')
+    getDBInfo(): string {
+        console.log(this.configService.get('logLevel'));
+        console.log(this.configService.get('apiVersion'));
+        return this.configService.get('dbInfo');
+    }
+
+    @Get('redis-info')
+    getRedisInfo(): string {
+        return `${this.configService.get('redis.host')}:${this.configService.get('redis.port')}`;
+    }
 }
--- src/Chapter9/src/main.ts
+++ src/Chapter9/src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 void (async () => {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
+    const configService = app.get(ConfigService);
+    await app.listen(configService.get('SERVER_PORT'), () =>
+        console.log(`Server is running on http://localhost:${configService.get('SERVER_PORT')}`),
+    );
 })();
